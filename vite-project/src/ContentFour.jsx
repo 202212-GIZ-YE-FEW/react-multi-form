9bca8db05e2a190f447568bc383b1c148604e97e
aseelflihan <aseelflihan@gmail.com>
2023-03-07 23:57:34 +0300

    complate

--- vite-project/src/ContentFour.jsx
+++ vite-project/src/ContentFour.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
-import Heading from './Heading'
+import React from 'react';
+import Heading from './Heading';
 
 function ContentFour() {
   return (
-    <div className='col-9' style={{ background: "#FFF", height: '100%' }}>
-    <div className="content">
-        <Heading title="Step 4" desc="Please provide your name, email address, and phone number." />
-       
+    <div className='col-lg-9 col-md-8 col-sm-12' style={{ background: "#FFF", height: '100%' }}>
+      <div className="content">
+        <Heading title="Finishing up" desc="Double-check everything looks oK before confirming." />
+
+        <div className="pricing">
+          <div className="item">
+            <div className="name">Arcade (Monthly)</div>
+            <div className="price">$9/mo</div>
+          </div>
+          <div className="item">
+            <div className="name">Online service</div>
+            <div className="quantity">1 Imo</div>
+          </div>
+          <div className="item">
+            <div className="name">Larger storage</div>
+            <div className="price">+$2/mo</div>
+          </div>
+          <div className="item total">
+            <div className="name">Total (per month)</div>
+            <div className="price">+$12/mo</div>
+          </div>
+        </div>
+
+        <div className="confirm-button">
+          <button>Confirm</button>
+        </div>
+
+      </div>
     </div>
-</div>
-  )
+  );
 }
 
-export default ContentFour
+export default ContentFour;
--- vite-project/src/ContentThree.jsx
+++ vite-project/src/ContentThree.jsx
@@ -1,14 +1,55 @@
-import React from 'react'
-import Heading from './Heading'
+import React from 'react';
+import Heading from './Heading';
+import "./styles.css"
+
 function ContentThree() {
   return (
-         <div className='col-9' style={{ background: "#FFF", height: '100%' }}>
-            <div className="content">
-                <Heading title="Step 3" desc="Please provide your name, email address, and phone number." />
+    <div className='col-lg-9 col-md-8 col-sm-12' style={{ background: "#FFF", height: '100%' }}>
+      <div className="content">
+        <Heading title="Pick add-ons" desc="Add-ons help enhance your gaming experience." />
 
+        <div className="content-three">
+          <div className="option-box">
+          <label>
+              <input type="checkbox" />
+              
+            </label>
+            <div>
+              <h3>Online service</h3>
+              <p className='p' >Access to multiplayer games <h4>+$1/mo</h4>  </p> 
             </div>
+          
+          </div>
+
+          <div className="option-box">
+          <label>
+              <input type="checkbox" />
+              
+            </label>
+            <div>
+              <h3>Larger storage</h3>
+              <p className='p'>Extra ITB of cloud save <h4>+$2/mo</h4> </p>
+              
+            </div>
+          
+          </div>
+
+          <div className="option-box">
+          <label>
+              <input type="checkbox" />
+             
+            </label>
+            <div>
+              <h3>Customizable Profile</h3>
+              <p className='p'>Custom theme on your profile <h4>+$2/mo</h4> </p>
+              
+            </div>
+          
+          </div>
         </div>
+      </div>
+    </div>
   )
 }
 
-export default ContentThree
+export default ContentThree;
